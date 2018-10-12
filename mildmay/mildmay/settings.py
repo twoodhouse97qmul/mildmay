f6b9e6a0d0664fdb3e9ef19a03e9868a8f133296
twoodhouse97qmul <aoterop97@gmail.com>
2018-10-12 13:04:47 +0100

    firssst3 commistss

--- mildmay/mildmay/settings.py
+++ mildmay/mildmay/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'mildmay/eroicamildmay/static/'
 
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
